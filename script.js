fda6a9ceecfa287e359878fb071db2a71ef8e57c
SoumyosishPal <soumyosishpal.108@gmail.com>
2025-09-18 13:13:05 +0530

    modified some changes in the webpage

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let targetNumber = Math.floor(Math.random() * 100) + 1;
 console.log("Target number:", targetNumber);
+
 function checkGuess() {
     let guess = parseInt(document.getElementById("guessInput").value);
     let feedback = document.getElementById("feedback");
+
+    if (isNaN(guess) || guess < 1 || guess > 100) {
+      feedback.textContent = "Please enter a valid number between 1 and 100.";
+      return;
+    }
+
     if (guess === targetNumber) {
         feedback.textContent = `Congratulations! You guessed the correct number ${targetNumber}!`;
     } else if (guess < targetNumber) {
@@ -11,4 +18,5 @@ function checkGuess() {
         feedback.textContent = `Try a smaller number.`;
     }
     document.getElementById("guessInput").value = "";
+    document.getElementById("guessInput").focus();
 }
